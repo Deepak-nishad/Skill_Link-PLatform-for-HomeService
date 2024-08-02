bbe543fa2db1a6e91dce5aa825870f06f8d1455b
Deepak-nishad <deepakkumar62559@gmail.com>
2024-08-02 15:36:06 +0530

    User and serverProvier table is create

--- Database/Skill_Link_DB.sql
+++ Database/Skill_Link_DB.sql
@@ -1,1 +1,31 @@
-use project;
+DROP DATABASE IF EXISTS skillLink;
+CREATE DATABASE skillLink;
+USE skillLink;
+
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS ServiceProviders;
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    NameFirst VARCHAR(50),
+    NameLast VARCHAR(50),
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    PhoneNumber VARCHAR(20),
+    Address VARCHAR(100)
+);
+
+CREATE TABLE ServiceProviders (
+    ServiceProviderID INT PRIMARY KEY AUTO_INCREMENT,
+    NameFirst VARCHAR(50),
+    NameLast VARCHAR(50),
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    PhoneNumber VARCHAR(20),
+	Skills Varchar(30),
+    Wages VARCHAR(50),
+    Address VARCHAR(100)
+);
+
+desc Users;
+desc ServiceProviders;
