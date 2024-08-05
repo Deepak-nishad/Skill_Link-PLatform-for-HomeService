89fffaef9908dd9cf878b02af07211e892a09de0
Deepak-nishad <deepakkumar62559@gmail.com>
2024-08-05 16:03:19 +0530

    Done with userRequierment table , Booking List, Feedback, status table

--- Database/Skill_Link_DB.sql
+++ Database/Skill_Link_DB.sql
@@ -29,3 +29,46 @@ CREATE TABLE ServiceProviders (
 
 desc Users;
 desc ServiceProviders;
+
+CREATE TABLE UserRequirements (
+	RequirementID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID INT,
+    Skills VARCHAR(100),
+    Wages VARCHAR(50),
+    Address VARCHAR(100),
+    Date DATE,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE Feedback (
+	Username VARCHAR(50),
+    ServiceProviderUsername VARCHAR(50) NOT NULL,
+    FeedbackMessage TEXT NOT NULL,
+    Rating INT NOT NULL,
+	FOREIGN KEY (ServiceProviderUsername) REFERENCES ServiceProviders(Username)
+);
+
+
+CREATE TABLE BookingList (
+	RequirementID INT,
+    UserID INT,
+   -- ServiceProviderID INT,--
+	Username VARCHAR(50) NOT NULL,
+    NameFirst VARCHAR(50),
+    NameLast VARCHAR(50),
+    PhoneNumber VARCHAR(10),
+	Skills VARCHAR(50),
+    Wages VARCHAR(50),
+    -- FOREIGN KEY (ServiceProviderID) REFERENCES ServiceProviders(ServiceProviderID),--
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+	FOREIGN KEY (Username) REFERENCES ServiceProviders(Username)
+);
+
+CREATE TABLE Status (
+    -- UserID INT,
+	RequirementID INT,
+	ServiceProviderUserName VARCHAR(50) NOT NULL UNIQUE,
+	IsSelected BOOLEAN,
+	FOREIGN KEY (ServiceProviderUsername) REFERENCES ServiceProviders(Username)
+);
+
